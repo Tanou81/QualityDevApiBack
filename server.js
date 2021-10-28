5d2c61d3b27d7c430167df6fe7be4763167aa97a
Romain.A <romain.aury@hotmail.com>
2021-10-28 16:03:56 +0200

    plein de requetes !

--- server.js
+++ server.js
@@ -1,34 +1,42 @@
-// mongoose.connect(
-//     process.env.MONGODB_URI,
-//     {
-//       useFindAndModify: false,
-//       useUnifiedTopology: true,
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//       server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-//       replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-//     },
-//     function (err) {
-//       if (err) return console.log("Error: ", err);
-//       console.log(
-//         "MongoDB Connection -- Ready state is:",
-//         mongoose.connection.readyState
-//       );
-//     }
-//   );
-
 const express = require("express");
-require("dotenv").config();
 const app = express();
+const mongoose = require("mongoose");
 
-app.use(express.static("public"))
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>")
-})
-app.get("/*", (req, res) => {
-    res.send("<h1>Hello World!</h1>")
-})
+// Routes import
+const userRoutes = require("./routes/userRoutes");
+const groupRoutes = require("./routes/groupRoutes");
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Listening on http://localhost:3000/")
-})
+app.use(express.json());
+app.use(express.static("public"));
+app.get("/", async (req, res) => {
+  res.send(
+    "<h1>API_QUALITE_DEV</h1>\
+      <p>Cette api fonctionne avec l'application \
+      <a target='_blank' href='https://qualite-dev.herokuapp.com'>\
+      QualiteDev</a>."
+  );
+});
+
+app.use("/api", userRoutes);
+app.use("/api", groupRoutes);
+
+console.log("Connecting to db");
+const MONGO_URI =
+  "mongodb+srv://romain:romain@projetqualitdev.fkfmr.mongodb.net/qualite_dev?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((req, res) => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on http://localhost:3000/");
+    });
+  });
+
+// db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//   console.log("connecté à Mongoose");
+// });
