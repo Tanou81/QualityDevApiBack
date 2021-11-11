564527b50be8ec565c80492a061dfcaccfd3c2de
EnzoMauger <maugerenzo1@gmail.com>
2021-11-11 21:21:51 +0100

    Debugging route /getstudentbyid (7)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -77,6 +77,8 @@ router.get("/getstudentbyemail", async (req, res) => {
 
 router.get("/getstudentbyid", async (req, res) => {
   const { _id } = req.body;
+  console.log("req.body");
+  console.log(req.body);
   try {
     const student = await User.find({ userType: 0, _id });
     console.log("/getstudentbyid id - student")
