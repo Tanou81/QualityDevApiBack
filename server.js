d00582fbc0777e5d1f848364543b57d1ff19db1a
Tanou81 <55876186+Tanou81@users.noreply.github.com>
2022-02-22 15:34:33 +0100

    tets

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const userRoutes = require("./routes/userRoutes");
 const groupRoutes = require("./routes/groupRoutes");
 const sprintRoutes = require("./routes/sprintRoutes");
 const labelformatRoutes = require("./routes/labelformatRoutes");
-const evaluationformatRoutes = require("./routes/evaluationformatRoutes");
+const evaluationFormatRoutes = require("./routes/evaluationFormatRoutes");
 // API Routes
 app.use("/api/user", userRoutes);
 app.use("/api/group", groupRoutes);
 app.use("/api/sprint", sprintRoutes);
 app.use("/api/labelformat", labelformatRoutes);
-app.use("/api/evaluationformatRoutes", evaluationformatRoutes);
+app.use("/api/evaluationformat", evaluationFormatRoutes);
 // Root query
 app.get("/", async (req, res) => {
   res.send(
