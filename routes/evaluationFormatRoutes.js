a53693dd5d4caea5fea492f9524e0a956586ecf5
Tanou81 <55876186+Tanou81@users.noreply.github.com>
2022-03-15 10:46:17 +0100

    updateevalFormat eval

--- routes/evaluationFormatRoutes.js
+++ routes/evaluationFormatRoutes.js
@@ -45,7 +45,7 @@ router.post("/updateevalformat", async (req, res) => {
   try{
   const Eval = await Evaluation.find({format : evaluationFormatId});
       console.log("Eval",Eval)
-      console.log("rétrécrir",Eval.grades)
+      console.log("rétrécrir",Eval[0].grades)
       // if( evalFormat.factors.length ()>Eval.grades.length ()){
       //   console.log("rétrécrirrr")
       // }
